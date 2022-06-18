9d0bef5d9610ecf99203b49cab4a5d5e248466c8
wuehr1999 <jonas.wuehr@t-online.de>
2022-06-18 21:16:29 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+##
+# Download lib with right version
+##
 git clone https://github.com/orocos/orocos_kinematics_dynamics.git
 cd orocos_kinematics_dynamics
 git checkout $2
 cd orocos_kdl
+
+##
+# Cleanup
+##
 rm -r build 
 rm -r install
 mkdir build
 mkdir install
 cd build
+
+##
+# Build
+##
 cmake .. -D CMAKE_INSTALL_PREFIX=../install -D CMAKE_C_COMPILER=$1-gcc -D CMAKE_CXX_COMPILER=$1-g++
 make -j$(nproc) install
 cd ../install
@@ -18,13 +29,17 @@ mkdir usr
 cp -r ../include usr/
 cp -r ../lib usr/
 cp -r ../share usr/
+
+##
+# Create package
+##
 mkdir DEBIAN
 cd DEBIAN
 echo "Package: kdl-107-$1
 Version: 0.3.13
 Architecture: all
-Maintainer: John Doe<your-email@gmail.com>
-Depengs:
+Maintainer: Jonas Wühr 
+Depends: libeigen3-dev 
 Homepage: https://github.com/shinokada/tera
 Description: 107-Systems kdl cross pkg." &> control
 cd ../../
