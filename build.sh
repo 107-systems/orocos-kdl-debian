c7ecdc2b5b6e1445031be111f43aadaf3f62af51
wuehr1999 <jonas.wuehr@t-online.de>
2022-06-19 22:21:11 +0200

    naming and control file fix

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 [arm-linux-gnueabihf | aarch64-linux-gnu] [orocos_kdl branch]" >&2
+  exit 1
+fi
 
 ##
 # Download lib with right version
@@ -23,27 +27,32 @@ cd build
 cmake .. -D CMAKE_INSTALL_PREFIX=../install -D CMAKE_C_COMPILER=$1-gcc -D CMAKE_CXX_COMPILER=$1-g++
 make -j$(nproc) install
 cd ../install
-mkdir kdl-107-$1
-cd kdl-107-$1
-mkdir usr
-cp -r ../include usr/
-cp -r ../lib usr/
-cp -r ../share usr/
 
 ##
 # Create package
 ##
+ARCHITECTURE='armhf'
+if [[ "$1" == *"aarch64"* ]]; then
+  ARCHITECTURE='arm64'
+fi
+NAME=liborocos-kdl-dev-$2-$(date '+%Y-%m-%d')-$ARCHITECTURE
+mkdir $NAME 
+cd $NAME
+mkdir usr
+cp -r ../include usr/
+cp -r ../lib usr/
+cp -r ../share usr/
 mkdir DEBIAN
 cd DEBIAN
-echo "Package: kdl-107-$1
-Version: 0.0.1
-Architecture: all
+echo "Package: $NAME
+Version: ${2:1} 
+Architecture: $ARCHITECTURE
 Maintainer: Jonas Wühr 
 Depends: libeigen3-dev 
-Homepage: - 
+Homepage: https://github.com/107-systems 
 Description: 107-Systems kdl cross pkg." &> control
 cd ../../
-sudo chown root:root -R kdl-107-$1
-dpkg-deb --build kdl-107-$1
+sudo chown root:root -R $NAME 
+dpkg-deb --build $NAME
 cd ../../../
 cp orocos_kinematics_dynamics/orocos_kdl/install/*.deb .
